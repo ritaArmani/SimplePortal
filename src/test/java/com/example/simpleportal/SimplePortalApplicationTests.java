91e4355e446d022019701e6ed7e65322fc056c4e
Jana <janaadham200527@gmail.com>
2026-05-08 20:48:49 +0300

    Initialize SimplePortal project with Maven setup and basic application structure

---
+++ src/test/java/com/example/simpleportal/SimplePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
